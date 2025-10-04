5e948677e9e583649fe53f9514ed0be178cf64a6
BAXA2004 <97600992+BAXA2004@users.noreply.github.com>
2025-10-04 18:07:53 +0000

    lab3_task3

--- src/lab3_task3.c
+++ src/lab3_task3.c
@@ -26,35 +26,35 @@ int my_strlen(const char *str);
 void my_strcpy(char *dest, const char *src);
 
 int main(void) {
-    // TODO: Test your functions here
-    char test[] = "Programming in C";
-    char copy[100];
+  // TODO: Test your functions here
+  char test[] = "Programming in C";
+  char copy[100];
 
-    int len = my_strlen(test);
-    printf("Length: %d\n", len);
+  int len = my_strlen(test);
+  printf("Length: %d\n", len);
 
-    my_strcpy(copy, test);
-    printf("Copy: %s\n", copy);
+  my_strcpy(copy, test);
+  printf("Copy: %s\n", copy);
 
-    return 0;
+  return 0;
 }
 
 // Implement functions below
 int my_strlen(const char *str) {
-    // TODO: count characters until '\0'
-    int length = 0;
-    while (str[length] != '\0') {
-        length++;
-    }
-    return length; // placeholder
+  // TODO: count characters until '\0'
+  int length = 0;
+  while (str[length] != '\0') {
+    length++;
+  }
+  return length;  // placeholder
 }
 
 void my_strcpy(char *dest, const char *src) {
-    // TODO: copy characters until '\0'
-    int i = 0;
-    while (src[i] != '\0') {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
+  // TODO: copy characters until '\0'
+  int i = 0;
+  while (src[i] != '\0') {
+    dest[i] = src[i];
+    i++;
+  }
+  dest[i] = '\0';
 }
